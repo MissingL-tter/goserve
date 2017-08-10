a4fa5bd3b9ddc527dba8c87d449530b9edefb8d1
MissingL-tter <434a44@gmail.com>
2017-08-10 18:58:22 -0400

    Initial Commit

---
+++ goserve.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	args := os.Args
+	path := "./"
+	port := ":8080"
+
+	if len(args) > 1 {
+		arg := strings.Split(args[1], ":")
+
+		if len(arg) > 0 {
+			path = arg[0]
+		}
+		if len(arg) > 1 {
+			port = ":" + arg[1]
+		}
+	}
+
+	fmt.Printf("Starting server at %v on port %v\n", path, port)
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(path))))
+}
+
+// goserve
+// goserve /foo/bar
+// goserve /foo/bar 8080
